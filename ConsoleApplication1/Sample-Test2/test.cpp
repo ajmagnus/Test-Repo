7e07efc20f3fd02e5d69a6ffe363aa21200b91e2
ajmagnus <45823834+ajmagnus@users.noreply.github.com>
2019-02-27 18:19:38 -0500

    Working Test Case

--- ConsoleApplication1/Sample-Test2/test.cpp
+++ ConsoleApplication1/Sample-Test2/test.cpp
@@ -1,20 +1,20 @@
 #include "pch.h"
 #include "../ConsoleApplication1/Message.h"
+#include <iostream>
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
 
-/*
+
 TEST(HelloWorldTest, test1) {
 	std::string output;
 	std::string answer;
-	Message source;
+	Message source = Message::Message();
 
 	output = source.print();
 	answer = "Hello World!";
-	EXPECT_TRUE(answer.compare(output));
+	EXPECT_EQ(output, answer);
 }
 
-*/
